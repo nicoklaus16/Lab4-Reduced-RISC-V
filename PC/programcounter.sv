5d6699e6e01715ff953d715a6c4119c672c0fdec
Nico <n.sanchez.f10@gmail.com>
2023-06-24 13:55:58 +0100

    cleaned up and reverified programcounter

--- PC/programcounter.sv
+++ PC/programcounter.sv
@@ -1,16 +1,15 @@
 module programcounter #(
-    parameter   immWIDTH = 32,
-                IMlength = 32
+    parameter   WIDTH = 32
 ) (
     input logic clk,
     input logic rst,
     input logic PCsrc,
-    input logic [immWIDTH-1:0]     ImmOp,
-    output logic [IMlength-1:0]    PC
+    input logic [WIDTH-1:0]     ImmOp,
+    output logic [WIDTH-1:0]    PC
 );
-    logic [IMlength-1:0] next_PC;
-    //logic [IMlength-1:0] branch_PC;
-    //logic [IMlength-1:0] inc_PC;
+    logic [WIDTH-1:0] next_PC;
+    //logic [WIDTH-1:0] branch_PC;
+    //logic [WIDTH-1:0] inc_PC;
 
     assign next_PC = PCsrc ? PC + ImmOp : PC + 3'b100;
 
@@ -22,7 +21,7 @@ module programcounter #(
     end*/
 
     always_ff @(posedge clk, posedge rst) begin
-        if (rst)    PC <= {IMlength{1'b0}};
+        if (rst)    PC <= {WIDTH{1'b0}};
         else        PC <= next_PC;
     end
 
